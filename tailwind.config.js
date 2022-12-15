6772030df8602f7605fd5e932f7cc6db6bb6d963
M-Husein <m.husein27@gmail.com>
2022-12-15 19:57:34 +0700

    remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
       minHeight: {
         'calc-nav': 'calc(100vh - 50px)', // 48px
       },
-      // minWidth: {
-      //   '50': '50%', //  !important
-      // },
       listStyleType: {
         'lower-alpha': 'lower-alpha',
       },
