75879ec07d3fc8ac2973b1421255ed696334a226
M-Husein <m.husein27@gmail.com>
2022-12-30 16:46:07 +0700

    fix: setup profile checkUserExits, responsive avatar menu position, styling card, etc

--- app/routes/profile/$userId.tsx
+++ app/routes/profile/$userId.tsx
@@ -1,4 +1,5 @@
-import { useState, useRef } from 'react'; // , useEffect
+import { useState, useRef, useEffect } from 'react'; // 
+import { type MetaFunction } from "@remix-run/node";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
@@ -23,15 +24,16 @@ import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
 import ExploreTwoToneIcon from '@mui/icons-material/ExploreTwoTone';
 import BadgeTwoToneIcon from '@mui/icons-material/BadgeTwoTone';
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
-import { useGetIdentity } from "@pankod/refine-core";
-
-// import { storage } from "~/utility"; // functions
-import useGetFileView from '~/utils/hooks/useGetFileView';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { useGetIdentity, useList } from "@pankod/refine-core";
+//
+import { storage } from "~/utility";
 import LayoutLogged from '~/components/LayoutLogged';
 import FormProfile from '~/components/profile/FormProfile';
 import Cover from '~/components/Cover';
 import AvatarSetup from '~/components/AvatarSetup';
-import Dropdown, { menuLeft } from '~/components/Dropdown';
+import Dropdown, { menuLeft, menuCenter } from '~/components/Dropdown';
 import WerkLogo from '~/svg/werk';
 import CameraIcon from '~/svg/Camera';
 import MoveIcon from '~/svg/Move';
@@ -40,11 +42,32 @@ import Education from '~/components/profile/sections/Education';
 import HardSkill from '~/components/profile/sections/HardSkill';
 import SoftSkill from '~/components/profile/sections/SoftSkill';
 import { enterToClick } from '~/utils/dom';
-import { INITIAL_BG } from '~/config';
+import { INITIAL_BG, BUCKET_ID, CANDIDATE_PROFILES } from '~/config';
+
+export const meta: MetaFunction = () => ({
+  title: "Profile | Werk",
+});
 
 const Profile: React.FC = () => {
   const { data: userData, isLoading } = useGetIdentity<any>(); // , isSuccess
-  const { $id: userId, $createdAt: createdAt, name: fullName } = userData || {}; // email: userEmail 
+  const { $id: userId, $createdAt: createdAt, name: fullName } = userData || {}; // email: userEmail
+  const { data: currentUser, isLoading: isLoadingCurrentUser } = useList({
+    liveMode: "off",
+    resource: CANDIDATE_PROFILES,
+    config: {
+      hasPagination: false,
+      filters: [
+        {
+          field: "candidateId",
+          operator: "eq",
+          value: userId,
+        },
+      ],
+    },
+  });
+  const { accountName, headLine, city, province, country, bio } = currentUser?.data?.[0] || {};
+  const theme = useTheme();
+  const isDeviceMd = useMediaQuery(theme.breakpoints.down('md'));
   const refFile = useRef();
   const [modalEdit, setModalEdit] = useState<boolean>(false);
   const [cover, setCover] = useState<any>(INITIAL_BG);
@@ -52,21 +75,16 @@ const Profile: React.FC = () => {
   const [avatarDefault, setAvatarDefault] = useState<any>();
   const [loadingAvatar, setLoadingAvatar] = useState<any>(true);
   const [openModalView, setOpenModalView] = useState<boolean>(false);
-  
-  useGetFileView(
-    userId ? userId + '_cropped' : null,
-    (fileUrl: any) => {
-      fileUrl && setAvatar(fileUrl.href);
-      setLoadingAvatar(false);
-    }
-  );
 
-  useGetFileView(
-    userId ? userId : null,
-    (fileUrl: any) => {
-      fileUrl && setAvatarDefault(fileUrl.href);
+  useEffect(() => {
+    if(userId){
+      const avatarSrc = storage.getFileView(BUCKET_ID, userId + '_cropped');
+      if(avatarSrc?.href){
+        setAvatar(avatarSrc.href);
+        setLoadingAvatar(false);
+      }
     }
-  );
+  }, [userId]);
 
   const onSaveProfile = (data: any) => {
     console.log('onSaveProfile data: ', data);
@@ -103,7 +121,7 @@ const Profile: React.FC = () => {
   }
 
   const viewPhotoAvatar = (close: any) => {
-    close();
+    close(); // Close menu
     setOpenModalView(true);
   }
 
@@ -112,12 +130,16 @@ const Profile: React.FC = () => {
   }
 
   const doDeleteAvatar = (close: any) => {
-    close();
+    close(); // Close menu
   }
 
   const doChangePositionAvatar = (close: any, openModalCrop: any) => {
-    close();
-    openModalCrop();
+    close(); // Close menu
+    const avatarOriSrc = storage.getFileView(BUCKET_ID, userId);
+    if(avatarOriSrc?.href){
+      setAvatarDefault(avatarOriSrc.href);
+    }
+    openModalCrop(); // Open dialog
   }
 
   return (
@@ -130,7 +152,6 @@ const Profile: React.FC = () => {
 
       <Container className="pb-7 md:pt-7 max-md:px-0">
         <Grid container spacing={3} justifyContent="center">
-          {/* lg={9} */}
           <Grid item lg={9} xs={12} className="flex flex-col gap-6">
             <Card variant="outlined" className="max-md:rounded-none">
               <Cover
@@ -144,7 +165,7 @@ const Profile: React.FC = () => {
                 <Grid container spacing={2}>
                   <Grid item lg={9} xs={12}>
                     <AvatarSetup
-                      loading={isLoading || loadingAvatar} // !avatar
+                      loading={isLoading || loadingAvatar}
                       disabled={isLoading || loadingAvatar}
                       avatarProps={{
                         sx: { width: 120, height: 120 },
@@ -170,7 +191,7 @@ const Profile: React.FC = () => {
                         avatar ?
                           <Dropdown
                             mountOnOpen // keepMounted
-                            {...menuLeft}
+                            {...(isDeviceMd ? menuCenter : menuLeft)}
                             disableAutoFocusItem
                             label={<CameraIcon />}
                             buttonProps={{
@@ -207,22 +228,28 @@ const Profile: React.FC = () => {
                           </Button>
                       )}
                     </AvatarSetup>
-
+                    
                     <h4 className="mb-0 mt-3 font-semibold text-gray-800">{fullName}</h4>
-                    <h6 className="mb-1 text-orange-400">Headline</h6>
-                    <address className="not-italic text-sm text-gray-500">
-                      <RoomTwoToneIcon sx={{ fontSize: 16 }} className="align-middle" /> Address
-                    </address>
-                  </Grid>
 
+                    {headLine && <h6 className="mb-1 text-orange-400">{headLine}</h6>}
+                    
+                    {(city || province || country) &&
+                      <address className="not-italic text-sm text-gray-500">
+                        <RoomTwoToneIcon sx={{ fontSize: 16 }} className="align-middle" /> {[city, province, country].filter((f: any) => f).join(', ')}
+                      </address>
+                    }
+                  </Grid>
+                  
                   <Grid item lg={3} xs={12} className="lg:text-right max-md:flex max-md:flex-col items-center">
-                    <Button
-                      color="secondary"
-                      className="mb-4 bg-gray-100 text-gray-500 hover:bg-blue-600 hover:text-white"
-                    >
-                      <WerkLogo className="mr-1" />
-                      werk.id/@john_doe
-                    </Button>
+                    {accountName &&
+                      <Button
+                        color="secondary"
+                        className="mb-4 bg-gray-100 text-gray-500 hover:bg-blue-600 hover:text-white"
+                      >
+                        <WerkLogo className="mr-1" />
+                        werk.id/@{accountName}
+                      </Button>
+                    }
                     <Button
                       className="text-blue-600 min-w-0"
                       onClick={onOpenModal}
@@ -233,11 +260,11 @@ const Profile: React.FC = () => {
                   </Grid>
                 </Grid>
 
-                <p className="my-5 text-sm text-gray-500">
-                  I'm a software engineer based in Barcelona with 10 years of experience in the software industry. 
-                  My focus area for the past few years has been front-end development with React, but I'm also skilled in back-end development with Go. 
-                  I am very quality-conscious because I spent my early career years in QA roles
-                </p>
+                {bio &&
+                  <p className="my-5 text-sm text-gray-500">
+                    {bio}
+                  </p>
+                }
                 <div className="text-xs text-gray-400">
                   Joined Werk: <time dateTime={createdAt} className="font-medium">23 November 2022</time>
                 </div>
@@ -252,9 +279,9 @@ const Profile: React.FC = () => {
 
             <SoftSkill />
           </Grid>
-          {/* lg={4} */}
+          
           <Grid item lg={3} xs={12} className="flex flex-col gap-6">
-            <Card variant="outlined" className="max-md:rounded-none">
+            <Card variant="outlined" className="max-md:rounded-none w-card">
               <CardHeader
                 avatar={<WorkTwoToneIcon />}
                 titleTypographyProps={{
@@ -267,54 +294,20 @@ const Profile: React.FC = () => {
                 className="py-2 border-bottom"
               />
               <List component="div" dense>
-                <ListItem
-                  // key={value}
-                  component="label"
-                  secondaryAction={
-                    <Checkbox
-                      size="small"
-                      edge="end"
-                      // onChange={handleToggle(value)}
-                      // checked={checked.indexOf(value) !== -1}
-                      // inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                  }
-                  disablePadding
-                >
-                  <ListItemButton className="px-5">
-                    <ListItemText primary="Internship" />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-              <CardActions className="justify-end border-top py-1">
-                <Button size="small" color="inherit">
-                  <HelpTwoToneIcon sx={{ fontSize: 15 }} className="mr-1" />
-                  Learn more
-                </Button>
-              </CardActions>
-            </Card>
-
-            <Card variant="outlined" className="max-md:rounded-none">
-              <CardHeader
-                avatar={<ExploreTwoToneIcon />}
-                titleTypographyProps={{
-                  className: "font-medium",
-                }}
-                title="Preferred Work Type"
-                className="py-2 border-bottom"
-              />
-              <List component="div" dense>
-                {['Onsite Work', 'Remote Work', 'Hybrid'].map((item: any) =>
+                {['Internship', 'Freelance Hourly Based'].map((item: any) =>
                   <ListItem
                     key={item}
+                    disablePadding
                     component="label"
                     secondaryAction={
                       <Checkbox
                         size="small"
                         edge="end"
+                        // onChange={handleToggle(value)}
+                        // checked={checked.indexOf(value) !== -1}
+                        // inputProps={{ 'aria-labelledby': labelId }}
                       />
                     }
-                    disablePadding
                   >
                     <ListItemButton className="px-5">
                       <ListItemText primary={item} />
@@ -330,7 +323,43 @@ const Profile: React.FC = () => {
               </CardActions>
             </Card>
 
-            <Card variant="outlined" className="max-md:rounded-none">
+            <Card variant="outlined" className="max-md:rounded-none w-card">
+              <CardHeader
+                avatar={<ExploreTwoToneIcon />}
+                titleTypographyProps={{
+                  className: "font-medium",
+                }}
+                title="Preferred Work Type"
+                className="py-2 border-bottom"
+              />
+              <List component="div" dense>
+                {['Onsite Work', 'Remote Work', 'Hybrid'].map((item: any) =>
+                  <ListItem
+                    key={item}
+                    disablePadding
+                    component="label"
+                    secondaryAction={
+                      <Checkbox
+                        size="small"
+                        edge="end"
+                      />
+                    }
+                  >
+                    <ListItemButton className="px-5">
+                      <ListItemText primary={item} />
+                    </ListItemButton>
+                  </ListItem>
+                )}
+              </List>
+              <CardActions className="justify-end border-top py-1">
+                <Button size="small" color="inherit">
+                  <HelpTwoToneIcon sx={{ fontSize: 15 }} className="mr-1" />
+                  Learn more
+                </Button>
+              </CardActions>
+            </Card>
+
+            <Card variant="outlined" className="max-md:rounded-none w-card">
               <CardHeader
                 avatar={<BadgeTwoToneIcon />}
                 titleTypographyProps={{
